4e3d67d6e178f1a7ebbba3bb52369186d97ee622
Oltean Petru <poltean33@gmail.com>
2023-12-11 10:20:50 +0100

    changede to be oop

--- src/palindrome.ts
+++ src/palindrome.ts
@@ -1,25 +1,43 @@
 import readline from 'readline';
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
+class App {
+  private rl: readline.Interface;
 
-const date = new Date();
-const hour = date.getHours();
-if (hour >= 6 && hour <= 18) {
-  console.log(`Bonjour!`);
-} else {
-  console.log(`Bonsoir!`);
+  constructor() {
+    this.rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout
+    });
+  }
+
+  greet() {
+    const date = new Date();
+    const hour = date.getHours();
+    if (hour >= 6 && hour <= 18) {
+      console.log(`Bonjour!`);
+    } else {
+      console.log(`Bonsoir!`);
+    }
+  }
+
+  askAndReverseWord() {
+    this.rl.question('Mot?: ', (word) => {
+      const reversedWord = word.split('').reverse().join('');
+      if (word === reversedWord) {
+        console.log(`${reversedWord} Bien Ouej!`);
+      } else {
+        console.log(`${reversedWord}`);
+      }
+      console.log('Bye!');
+      this.rl.close();
+    });
+  }
+
+  run() {
+    this.greet();
+    this.askAndReverseWord();
+  }
 }
 
-rl.question('Mot?: ', (word) => {
-  const reversedWord = word.split('').reverse().join('');
-  if (word === reversedWord) {
-    console.log(`${reversedWord} Bien Ouej!`);
-  } else {
-    console.log(`${reversedWord}`);
-  }
-  console.log('Bye!');
-  rl.close();
-});
+const app = new App();
+app.run();
